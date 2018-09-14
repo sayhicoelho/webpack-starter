e0196eff0972fd18083d4cb8a95bce4a2ee9cf11
Renan Coelho <sayhicoelho@gmail.com>
2018-09-14 15:04:00 -0300

    Move bundle.js to js/bundle.js

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: ['@babel/polyfill', './src/main.js'],
   output: {
     path: path.resolve(__dirname, '../dist'),
-    filename: 'bundle.js'
+    filename: 'js/bundle.js'
   },
   plugins: [
     new Dotenv({ safe: true }),
